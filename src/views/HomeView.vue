5ae6beff4465b06443d00eaa72d4a6db08692aae
Megan Luu <meganluu@Megans-MacBook-Air-41.local>
2023-03-29 16:12:29 -0700

    uniform tabs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@
       <v-toolbar-title>Scenario Creation</v-toolbar-title>
     </v-toolbar>
     <div class="d-flex flex-row">
+      <div class="p-2">
       <v-tabs
         v-model="tab"
         direction="vertical"
@@ -42,9 +43,12 @@
           Capabilities
         </v-tab>
       </v-tabs>
+      </div>
       <v-window v-model="tab">
+        <v-main>
         <v-window-item value="option-1">
           <v-card>
+            <v-card-text>
               <v-form
               ref="form"
               v-model="valid"
@@ -57,7 +61,7 @@
               label="Instructor Type"
               required
             ></v-select>
-
+              </v-form>
             <v-text-field
               v-model="scenario_props['name']"
               label="Scenario Name"
@@ -72,14 +76,15 @@
               chips
               hint='Click "enter" to add multiple items'
             ></v-combobox>
-            </v-form>
+
+            </v-card-text>
           </v-card>
         </v-window-item>
+        </v-main>
 
         <v-window-item value="option-2">
           <v-card flat>
-            <v-card-text>
-              <v-form>
+              <v-card-text>
                 <v-text-field
               v-model="patient_props['name']"
               :rules="nameRules"
@@ -122,27 +127,7 @@
               label="Weight [kg]"
               required
             ></v-text-field>
-            </v-form>
-              <p>
-                Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nunc sed turpis.
-              </p>
-
-              <p>
-                Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra condimentum.
-              </p>
-
-              <p>
-                Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae tortor. Aliquam eu nunc.
-              </p>
-
-              <p>
-                Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce fermentum odio nec arcu.
-              </p>
-
-              <p class="mb-0">
-                Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi.
-              </p>
-            </v-card-text>
+            </v-card-text> 
           </v-card>
         </v-window-item>
         <v-window-item value="option-3">
@@ -196,13 +181,6 @@
               chips
               hint='Click "enter" to add multiple items'
             ></v-combobox>
-              <p>
-                Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor, justo.
-              </p>
-
-              <p class="mb-0">
-                Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
-              </p>
             </v-card-text>
           </v-card>
         </v-window-item>
