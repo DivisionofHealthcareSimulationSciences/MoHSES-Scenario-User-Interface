ad35ec0fa8665640955b572a8b3a218f112fcce9
Megan Luu <meganluu@Megans-MacBook-Air-41.local>
2023-03-29 14:47:47 -0700

    tabs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -305,13 +305,7 @@
               chips
               hint='Click "enter" to add multiple items'
             ></v-combobox>
-              <p>
-                Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor, justo.
-              </p>
 
-              <p class="mb-0">
-                Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
-              </p>
             </v-card-text>
           </v-card>
         </v-window-item>
