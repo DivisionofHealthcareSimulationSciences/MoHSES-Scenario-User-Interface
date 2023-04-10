39dbc3fd19f6d7ac74b1a24f01c8548034dcbec9
Jake Flynn <jflynn56@uw.edu>
2023-04-10 15:21:01 -0700

    New pages

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,23 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+  path: '/MoHSES',
+    name: 'MoHSES',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "MoHSES" */ '../views/MoHSESView.vue')
+  },
+  {
+    path: '/BioGears',
+      name: 'BioGears',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "BioGears" */ '../views/BioGearsView.vue')
+    }
 ]
 
 const router = createRouter({
