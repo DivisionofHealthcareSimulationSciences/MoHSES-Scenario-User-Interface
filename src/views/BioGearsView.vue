384c14d972c6cc66db8719648178792495c3882c
Megan Luu <meganluu@Megans-MacBook-Air-41.local>
2023-04-10 15:50:27 -0700

    updated tabs

--- src/views/BioGearsView.vue
+++ src/views/BioGearsView.vue
@@ -4,6 +4,45 @@
     >
       <v-toolbar-title>BioGears Scenario Creation Tool</v-toolbar-title>
     </v-toolbar>
+    <v-row>
+    <v-col cols="3">
+      <v-tabs
+        v-model="tab"
+        direction="vertical"
+        color="primary"
+      >
+        <v-tab value="home">
+          <v-icon start>
+            mdi-home
+          </v-icon>
+          About this page
+        </v-tab>
+        <v-tab value="option-1">
+          <v-icon start>
+            mdi-account
+          </v-icon>
+          Patient Information
+        </v-tab>
+        <v-tab value="option-2">
+          <v-icon start>
+            mdi-access-point
+          </v-icon>
+          Patient Vitals
+        </v-tab>
+        <v-tab value="option-3">
+          <v-icon start>
+            mdi-list-status
+          </v-icon>
+          Head-to-toe
+        </v-tab>
+
+        
+      </v-tabs>
+    
+    </v-col>
+  </v-row>
+
+
     </template>
 <script>
   export default {
