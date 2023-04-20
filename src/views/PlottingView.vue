2177ddb7928acb44839914f2242bde4ac8946357
Jake Flynn <jflynn56@uw.edu>
2023-04-19 20:48:20 -0700

    d3

--- src/views/PlottingView.vue
+++ src/views/PlottingView.vue
@@ -1,27 +1,18 @@
 <template>
     <v-toolbar style="color: white" color="#4b0082">
-       <v-toolbar-title>BioGears Plotting Tool</v-toolbar-title>
+      <v-toolbar-title>BioGears Plotting Tool</v-toolbar-title>
     </v-toolbar>
     <v-main class="bg-grey-lighten-3">
-       <v-container>
-          <v-card class="mt-n12">
-             <v-card-text>
-                <p style="font-size: 20px">Please upload your .CSV:</p>
-                <br>
-                <v-file-input
-                   v-model="file"
-                   accept=".csv"
-                   label="Upload CSV"
-                   @change="onFileSelected"
-                   ></v-file-input>
-             </v-card-text>
-          </v-card>
-       </v-container>
+      <v-container>
+        <v-card class="mt-n12">
+          <v-card-text>
+            <p style="font-size: 20px">Please upload your .CSV:</p>
+            <br>
+            <v-file-input v-model="file"></v-file-input>
+          </v-card-text>
+        </v-card>
+      </v-container>
     </v-main>
- </template>
- <script>
-    export default {
-      data: () => ({
-      })
-    }
- </script>
+  </template>
+  <script>
+  </script>
